22cdbe3e67c6d953f0f878a5707def8d52d10b00
Tuomas <haarnoja@berkeley.edu>
2017-11-20 20:06:19 -0800

    Add option for extra folders in the launch script.

--- sac/misc/instrument.py
+++ sac/misc/instrument.py
@@ -3,25 +3,38 @@ import uuid
 
 from rllab.misc.instrument import run_experiment_lite
 
-SAC_PATH = os.path.dirname(
-    os.path.realpath(os.path.join(__file__, '..', '..', 'sac')))
+from sac.misc.utils import PROJECT_PATH
 
 
-def run_sac_experiment(*args, mode, **kwargs):
+def _create_symlink(folder):
+    # Create a symbolic link that points to the sac folder and include it
+    # in the tarball.
+
+    # Unique filename for the symlink.
+    include_path = os.path.join('/tmp/', str(uuid.uuid4()))
+    os.makedirs(include_path)
+
+    os.symlink(os.path.join(PROJECT_PATH, folder),
+               os.path.join(include_path, folder))
+
+    return include_path
+
+
+def run_sac_experiment(main, mode, include_folders=None, **kwargs):
+    if include_folders is None:
+        include_folders = list()
 
     if mode == 'ec2':
-        # Create a symbolic link that points to the sac folder and include it
-        # in the tarball.
+        include_folders.append('sac')
+        all_symlinks = list()
 
-        # Unique filename for the symlink.
-        sac_path = os.path.join('/tmp/', str(uuid.uuid4()))
-        os.makedirs(sac_path)
-        os.symlink(SAC_PATH, os.path.join(sac_path, 'sac'))
+        for folder in include_folders:
+            all_symlinks.append(_create_symlink(folder))
 
-        kwargs.update(added_project_directories=[sac_path])
+        kwargs.update(added_project_directories=all_symlinks)
 
     run_experiment_lite(
+        stub_method_call=main,
         mode=mode,
-        *args,
         **kwargs,
     )
--- sac/misc/utils.py
+++ sac/misc/utils.py
@@ -1,7 +1,11 @@
 import datetime
 import dateutil.tz
+import os
 
 
 def timestamp():
     now = datetime.datetime.now(dateutil.tz.tzlocal())
     return now.strftime('%Y-%m-%d-%H-%M-%S-%f-%Z')
+
+PROJECT_PATH = os.path.dirname(
+    os.path.realpath(os.path.join(__file__, '..', '..')))
