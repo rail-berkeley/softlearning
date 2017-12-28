49c887e9208d2a0119eba2a26e38d033bb2ba4da
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-12-28 15:21:44 -0800

    Update RealNVPPolicy to use RealNVPBijector

--- sac/policies/real_nvp.py
+++ sac/policies/real_nvp.py
@@ -4,62 +4,65 @@ from contextlib import contextmanager
 import numpy as np
 import tensorflow as tf
 
-from rllab.misc.overrides import overrides
-from rllab.misc import logger
-from rllab.core.serializable import Serializable
+from sac.distributions import RealNVPBijector
 
-from sac.distributions import RealNVP
-from sac.policies import NNPolicy
-
-
-class RealNVPPolicy(NNPolicy, Serializable):
+class RealNVPPolicy(object):
     """Real NVP policy"""
 
-    def __init__(self, env_spec, real_nvp_config, squash=True, qf=None):
+    def __init__(self, env_spec, real_nvp_config=None, squash=True, qf=None):
         """Initialize Real NVP policy.
 
         Args:
             env_spec (`rllab.EnvSpec`): Specification of the environment
                 to create the policy for.
-            real_nvp_config (`sac.distributions.real_nvp.Config`): Parameter
+            real_nvp_config (`dict`): Parameter
                 configuration for real nvp distribution.
             squash (`bool`): If True, squash the action samples between
                 -1 and 1 with tanh.
             qf (`ValueFunction`): Q-function approximator.
         """
-        Serializable.quick_init(self, locals())
-
         self.real_nvp_config = real_nvp_config
+
+        self._env_spec = env_spec
         self._Da = env_spec.action_space.flat_dim
         self._Ds = env_spec.observation_space.flat_dim
         self._qf = qf
 
         self._observations_ph = tf.placeholder(
             dtype=tf.float32,
-            shape=[None, self._Ds],
+            shape=(None, self._Ds),
             name='observations',
         )
 
-        self.distribution = RealNVP(real_nvp_config)
+        ds = tf.contrib.distributions
+        self.bijector = RealNVPBijector(config=real_nvp_config,
+                                        event_ndims=self._Ds)
+        self.base_distribution = ds.MultivariateNormalDiag(
+            loc=tf.zeros(self._Ds), scale_diag=tf.ones(self._Ds))
 
-        super().__init__(
-            env_spec,
-            obs_pl=self._observations_ph,
-            action=(
-                tf.tanh(self._distribution_z)
-                if squash
-                else self._distribution_z
-            ),
-            scope_name='policy'
-        )
+        self.distribution = ds.TransformedDistribution(
+            distribution=self.base_distribution,
+            bijector=self.bijector,
+            name="RealNVPPolicyDistribution")
 
-    @overrides
+
+        y = self.distribution.bijector.forward(self._observations_ph)
+        self.log_pi = self.distribution.log_prob(y)
+        self.pi = tf.exp(self.log_pi)
+        self._action = tf.tanh(self.pi) if squash else self.pi
+
     def get_action(self, observations):
         """Sample action based on the observations.
 
         TODO: implement
         """
-        return super().get_action(observations)
+        return super().get_action(observations[None])[0], None
+
+    def get_actions(self, observations):
+        feed_dict = {self._observations_ph: observations}
+        actions = tf.get_default_session().run(
+            self._action, feed_dict=feed_dict)
+        return actions
 
     def log_diagnostics(self, batch):
         """Record diagnostic information to the logger.
