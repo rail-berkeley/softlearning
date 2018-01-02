6ef61114b9f5b079b732b5b99c5ea6d1f9957df9
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-01-01 20:13:15 -0800

    _maybe_assert_valid_y -> _maybe_assert_valid_x

--- sac/distributions/real_nvp_bijector.py
+++ sac/distributions/real_nvp_bijector.py
@@ -369,7 +369,7 @@ class RealNVPBijector(ConditionalBijector):
         """TODO"""
         if not self.validate_args:
             return x
-        raise NotImplementedError("_maybe_assert_valid_y")
+        raise NotImplementedError("_maybe_assert_valid_x")
 
     def _maybe_assert_valid_y(self, y):
         """TODO"""
