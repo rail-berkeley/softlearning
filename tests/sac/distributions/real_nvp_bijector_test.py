d12a8ea61b6a2b19b2c5e1d253583b8e64b7b288
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-01-05 18:01:41 -0800

    Adds tests for multi dimensional CouplingBijector inputs/outputs

--- tests/sac/distributions/real_nvp_bijector_test.py
+++ tests/sac/distributions/real_nvp_bijector_test.py
@@ -73,6 +73,56 @@ class CouplingBijectorTest(test.TestCase):
         self.assertAllEqual(even_forward_log_det_jacobian_num,
                             SCALE_FN_WITH_BIAS(odd_forward_out_num[:, 0]))
 
+    def test_multi_dimensional_composed_forward_and_log_det_jacobian(self):
+        odd_layer = CouplingBijector(
+            parity="odd",
+            name="coupling_odd",
+            translation_fn=TRANSLATION_FN_WITH_BIAS,
+            scale_fn=SCALE_FN_WITH_BIAS
+        )
+
+        even_layer = CouplingBijector(
+            parity="even",
+            name="coupling_even",
+            translation_fn=TRANSLATION_FN_WITH_BIAS,
+            scale_fn=SCALE_FN_WITH_BIAS
+        )
+
+        inputs = tf.constant(np.random.rand(32, 13))
+        odd_forward_out = odd_layer.forward(inputs)
+        odd_forward_log_det_jacobian = odd_layer.forward_log_det_jacobian(
+            inputs)
+
+        even_forward_out = even_layer.forward(odd_forward_out)
+        even_forward_log_det_jacobian = even_layer.forward_log_det_jacobian(
+            odd_forward_out)
+
+        # Verify that the true side of the mask comes out as identity
+        with self.test_session() as session:
+            (inputs_num,
+             odd_forward_out_num,
+             even_forward_out_num,
+             odd_forward_log_det_jacobian_num,
+             even_forward_log_det_jacobian_num) = session.run((
+                 inputs,
+                 odd_forward_out,
+                 even_forward_out,
+                 odd_forward_log_det_jacobian,
+                 even_forward_log_det_jacobian,
+             ))
+
+        self.assertAllEqual(odd_forward_out_num[:, 1::2],
+                            inputs_num[:, 1::2])
+        self.assertAllEqual(even_forward_out_num[:, 0::2],
+                            odd_forward_out_num[:, 0::2])
+
+        self.assertAllClose(
+            odd_forward_log_det_jacobian_num,
+            np.sum(SCALE_FN_WITH_BIAS(inputs_num[:, 1::2]), axis=1))
+        self.assertAllClose(
+            even_forward_log_det_jacobian_num,
+            np.sum(SCALE_FN_WITH_BIAS(odd_forward_out_num[:, 0::2]), axis=1))
+
     def test_composed_inverse_and_log_det_jacobian(self):
         odd_layer = CouplingBijector(
             parity="odd",
@@ -118,6 +168,56 @@ class CouplingBijectorTest(test.TestCase):
                             -SCALE_FN_WITH_BIAS(inputs_num[:, 1]))
         self.assertAllEqual(even_inverse_log_det_jacobian_num,
                             -SCALE_FN_WITH_BIAS(odd_inverse_out_num[:, 0]))
+
+    def test_multi_dimensional_composed_inverse_and_log_det_jacobian(self):
+        odd_layer = CouplingBijector(
+            parity="odd",
+            name="coupling_odd",
+            translation_fn=TRANSLATION_FN_WITH_BIAS,
+            scale_fn=SCALE_FN_WITH_BIAS
+        )
+
+        even_layer = CouplingBijector(
+            parity="even",
+            name="coupling_even",
+            translation_fn=TRANSLATION_FN_WITH_BIAS,
+            scale_fn=SCALE_FN_WITH_BIAS
+        )
+
+        inputs = tf.constant(np.random.rand(32, 13))
+        odd_inverse_out = odd_layer.inverse(inputs)
+        odd_inverse_log_det_jacobian = odd_layer.inverse_log_det_jacobian(
+            inputs)
+
+        even_inverse_out = even_layer.inverse(odd_inverse_out)
+        even_inverse_log_det_jacobian = even_layer.inverse_log_det_jacobian(
+            odd_inverse_out)
+
+        # Verify that the true side of the mask comes out as identity
+        with self.test_session() as session:
+            (inputs_num,
+             odd_inverse_out_num,
+             even_inverse_out_num,
+             odd_inverse_log_det_jacobian_num,
+             even_inverse_log_det_jacobian_num) = session.run((
+                 inputs,
+                 odd_inverse_out,
+                 even_inverse_out,
+                 odd_inverse_log_det_jacobian,
+                 even_inverse_log_det_jacobian,
+             ))
+
+        self.assertAllEqual(odd_inverse_out_num[:, 1::2],
+                            inputs_num[:, 1::2])
+        self.assertAllEqual(even_inverse_out_num[:, 0::2],
+                            odd_inverse_out_num[:, 0::2])
+
+        self.assertAllClose(
+            odd_inverse_log_det_jacobian_num,
+            np.sum(-SCALE_FN_WITH_BIAS(inputs_num[:, 1::2]), axis=1))
+        self.assertAllClose(
+            even_inverse_log_det_jacobian_num,
+            np.sum(-SCALE_FN_WITH_BIAS(odd_inverse_out_num[:, 0::2]), axis=1))
 
     def test_forward_without_bias(self):
         layer_without_bias = CouplingBijector(
