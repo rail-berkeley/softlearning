5da36a08161199c41608fa62097382735a04c0e3
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-02-03 14:25:08 -0800

    Update `RandomGoalSwimmerEnv.log_diagnostics`
    * This should be cleaned in the future

--- sac/envs/random_goal_swimmer_env.py
+++ sac/envs/random_goal_swimmer_env.py
@@ -94,22 +94,40 @@ class RandomGoalSwimmerEnv(SwimmerEnv):
         """
         if len(paths) > 0:
             progs = [
-                path["observations"][-1][-5] - path["observations"][0][-5]
+                np.linalg.norm(path["observations"][-1][-5:-3]
+                               - path["observations"][0][-5:-3])
                 for path in paths
             ]
-            logger.record_tabular('AverageForwardProgress', np.mean(progs))
-            logger.record_tabular('MaxForwardProgress', np.max(progs))
-            logger.record_tabular('MinForwardProgress', np.min(progs))
-            logger.record_tabular('StdForwardProgress', np.std(progs))
-        else:
-            logger.record_tabular('AverageForwardProgress', np.nan)
-            logger.record_tabular('MaxForwardProgress', np.nan)
-            logger.record_tabular('MinForwardProgress', np.nan)
-            logger.record_tabular('StdForwardProgress', np.nan)
+            logger.record_tabular('AverageProgress', np.mean(progs))
+            logger.record_tabular('MaxProgress', np.max(progs))
+            logger.record_tabular('MinProgress', np.min(progs))
+            logger.record_tabular('StdProgress', np.std(progs))
 
-        logger.record_tabular('FinalDistanceFromGoal', self.goal_distance)
-        origin_distance_from_goal = np.sqrt(np.sum(self.goal_position**2))
-        logger.record_tabular('OriginDistanceFromGoal',
-                              origin_distance_from_goal)
-        progress_towards_goal = origin_distance_from_goal - self.goal_distance
-        logger.record_tabular('ProgressTowardsGoal', progress_towards_goal)
+            goal_positions, final_positions = zip(*[
+                (p['observations'][-1][-2:], p['observations'][-1][-5:-3])
+                for p in paths
+            ])
+
+            begin_goal_distances = [
+                np.linalg.norm(goal_position) for goal_position in goal_positions]
+            final_goal_distances = [
+                np.linalg.norm(goal_position - final_position)
+                for goal_position, final_position in zip(goal_positions, final_positions)
+            ]
+            progress_towards_goals = [
+                begin_goal_distance - final_goal_distance
+                for (begin_goal_distance, final_goal_distance)
+                in zip(begin_goal_distances, final_goal_distances)
+            ]
+
+
+            for series, name in zip((begin_goal_distances,
+                                     final_goal_distances,
+                                     progress_towards_goals),
+                                    ('BeginGoalDistance',
+                                     'FinalGoalDistance',
+                                     'ProgressTowardsGoal')):
+                for fn_name in ('mean', 'std', 'min', 'max'):
+                    fn = getattr(np, fn_name)
+                    logger.record_tabular(fn_name.capitalize() + name,
+                                          fn(series))
