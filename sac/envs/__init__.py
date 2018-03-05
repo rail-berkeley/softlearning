f0547a115b73cfa0717d83d3d2537fd8f1774525
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-03-04 16:09:44 -0800

    Add CrossMazeAntEnv

--- sac/envs/__init__.py
+++ sac/envs/__init__.py
@@ -3,7 +3,10 @@ from .random_goal_swimmer_env import RandomGoalSwimmerEnv
 from .random_goal_ant_env import RandomGoalAntEnv
 from .random_goal_humanoid_env import RandomGoalHumanoidEnv
 from .random_wall_ant_env import RandomWallAntEnv
+
+from .cross_maze_ant_env import CrossMazeAntEnv
 from .simple_maze_ant_env import SimpleMazeAntEnv
+
 from .hierarchy_proxy_env import HierarchyProxyEnv
 from .multi_direction_ant_env import MultiDirectionAntEnv
 from .multi_direction_humanoid_env import MultiDirectionHumanoidEnv
