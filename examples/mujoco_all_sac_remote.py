1414ec025c2829f535830cdbd41145dca615e79b
Aurick Zhou <azhou42@berkeley.edu>
2018-04-08 15:52:55 -0700

    removed configs from script

--- examples/mujoco_all_sac_remote.py
+++ examples/mujoco_all_sac_remote.py
@@ -2,7 +2,6 @@ import argparse
 
 from rllab.envs.normalized_env import normalize
 from rllab.misc.instrument import VariantGenerator
-from rllab import config
 
 from sac.algos import SAC
 from sac.envs.gym_env import GymEnv
@@ -14,17 +13,14 @@ from sac.policies.gmm import GMMPolicy
 from sac.replay_buffers import SimpleReplayBuffer
 from sac.value_functions import NNQFunction, NNVFunction
 
-config.DOCKER_IMAGE = "haarnoja/sac"  # needs psutils
-config.AWS_IMAGE_ID = "ami-a3a8b3da"  # with docker already pulled
-
 COMMON_PARAMS = {
-    "seed": [1, 11, 21],
-    "lr": [1E-4, 3E-4],
+    "seed": [1, 2, 3],
+    "lr": [3E-4],
     "discount": 0.99,
     "tau": 0.01,
-    "K": 1,
-    "layer_size": 256,
-    "batch_size": 256,
+    "K": 4,
+    "layer_size": 128,
+    "batch_size": 128,
     "max_pool_size": 1E6,
     "n_train_repeat": 1,
     "epoch_length": 1000,
@@ -70,7 +66,7 @@ ENV_PARAMS = {
         'env_name': 'Ant-v1',
         'max_path_length': 1000,
         'n_epochs': 10000,
-        'scale_reward': [3,5,10],
+        'scale_reward': 3,
     },
     'humanoid': { # 21 DoF
         'prefix': 'humanoid',
