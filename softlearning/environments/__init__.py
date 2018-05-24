85ad0effc361f1d52906e2f72a750aecf9059a19
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-05-23 19:39:55 -0700

    Merge environments
    * Move environments over from sac to softlearning
    * Move xml files from sac/mujoco_models to models/

--- softlearning/environments/__init__.py
+++ softlearning/environments/__init__.py
@@ -1,3 +1,11 @@
 from .multigoal import MultiGoalEnv
 from .gym_env import GymEnv
 from .delayed_env import DelayedEnv
+from .multi_direction_env import (
+    MultiDirectionSwimmerEnv,
+    MultiDirectionAntEnv,
+    MultiDirectionHumanoidEnv)
+
+from .random_goal_ant_env import RandomGoalAntEnv
+from .cross_maze_ant_env import CrossMazeAntEnv
+from .hierarchy_proxy_env import HierarchyProxyEnv
--- softlearning/environments/simple_maze_ant_env.py
+++ softlearning/environments/simple_maze_ant_env.py
@@ -5,12 +5,11 @@ import os
 import numpy as np
 
 from rllab.core.serializable import Serializable
-from sac.misc.utils import PROJECT_PATH
+from softlearning.misc.utils import PROJECT_PATH
 from .helpers import random_point_in_circle, get_random_goal_logs
 from .random_goal_ant_env import RandomGoalAntEnv
 
-MODELS_PATH = os.path.abspath(
-    os.path.join(PROJECT_PATH, 'sac/mujoco_models'))
+MODELS_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'models'))
 
 class SimpleMazeAntEnv(RandomGoalAntEnv, Serializable):
     """Implements an ant whose goal is to reach a target in a maze"""
