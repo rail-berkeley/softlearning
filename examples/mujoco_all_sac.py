afcarl/sac

made sac with min 2 qf's default

examples/mujoco_all_sac.py
@@ -12,9 +12,7 @@ from rllab.envs.mujoco.humanoid_env import HumanoidEnv
 from rllab.misc.instrument import VariantGenerator
 from rllab import config
 
-# from sac.algos import SAC
-# move these into main SAC
-from sac.algos.sac_min_q_vf import SAC
+from sac.algos import SAC
 from sac.envs import (
     GymEnv,
     MultiDirectionSwimmerEnv,

rail-berkeley/softlearning

examples/mujoco_all_sac.py
@@ -11,9 +11,7 @@ from rllab.envs.mujoco.ant_env import AntEnv
 from rllab.envs.mujoco.humanoid_env import HumanoidEnv
 from rllab.misc.instrument import VariantGenerator
 
-# from softlearning.algorithms import SAC
-# move these into main SAC
-from softlearning.algorithms.sac_min_q_vf import SAC
+from softlearning.algorithms import SAC
 from softlearning.environments import (
     GymEnv,
     MultiDirectionSwimmerEnv,