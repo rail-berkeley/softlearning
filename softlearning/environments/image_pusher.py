6ae604e8c8b0d377d69480dddac7887c4730d0ef
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-07-10 11:27:20 -0700

    Change image pusher image resizing to happen directly in MjViewer

--- softlearning/environments/image_pusher.py
+++ softlearning/environments/image_pusher.py
@@ -17,29 +17,21 @@ class ImagePusherEnv(PusherEnv):
         Serializable.quick_init(self, locals())
         self.image_size = image_size
         PusherEnv.__init__(self, *args, **kwargs)
+        self.viewer_setup()
 
-    @overrides
     def get_current_obs(self):
-        # image = self.render(mode='rgb_array',)
-        data, width, height = self.get_viewer().get_image()
-        image = np.fromstring(
-            data, dtype='uint8'
-        ).reshape(height, width, 3)[::-1,:,:]
-        # resized_image = resize(image, size=(32, 32, 3))
-        resized_image = resize(
-            image,
-            output_shape=self.image_size,
-            preserve_range=True,
-            anti_aliasing=True)
+        self.viewer_setup()
+        image = self.render(mode='rgb_array')
 
-        # show_image = False
+        # image = resize(image, output_shape=(16,16,3), preserve_range=True)
+        # show_image = True
         # if show_image:
         #     from PIL import Image
         #     Image.fromarray(image.astype('uint8')).show()
         #     # .save(
         #     #     '/Users/kristian/code/softqlearning-private/tmp/{}_real.png'.format(self.i)
         #     # )
-        #     Image.fromarray(resized_image.astype('uint8')).show()
+        #     # Image.fromarray(resized_image.astype('uint8')).show()
         #     # .save(
         #     #     '/Users/kristian/code/softqlearning-private/tmp/{}_resized.png'.format(self.i)
         #     # )
@@ -47,14 +39,16 @@ class ImagePusherEnv(PusherEnv):
         #     from pdb import set_trace; from pprint import pprint; set_trace()
 
         return np.concatenate([
-            resized_image.reshape(-1),
+            image.reshape(-1),
             self.model.data.qpos.flat[self.JOINT_INDS],
             self.model.data.qvel.flat[self.JOINT_INDS],
         ]).reshape(-1)
 
     def get_viewer(self):
         if self.viewer is None:
-            self.viewer = MjViewer(visible=False)
+            width, height = self.image_size[:2]
+            self.viewer = MjViewer(
+                visible=False, init_width=width, init_height=height)
             self.viewer.start()
             self.viewer.set_model(self.model)
         return self.viewer
@@ -62,11 +56,14 @@ class ImagePusherEnv(PusherEnv):
     def viewer_setup(self):
         viewer = self.get_viewer()
         viewer.cam.trackbodyid = 0
-        viewer.cam.distance = 4.0
         cam_dist = 3.5
         cam_pos = np.array([0, 0, 0, cam_dist, -90, 0])
-        viewer.cam.lookat[:3] = cam_pos[:3]
-        viewer.cam.distance = cam_pos[3]
-        viewer.cam.elevation = cam_pos[4]
-        viewer.cam.azimuth = cam_pos[5]
+        viewer.cam.lookat[:3] = [0,0,0]
+        viewer.cam.distance = cam_dist
+        viewer.cam.elevation = -90
+        viewer.cam.azimuth = 0
         viewer.cam.trackbodyid = -1
+
+    def render(self, *args, **kwargs):
+        self.viewer_setup()
+        return super(ImagePusherEnv, self).render(*args, **kwargs)
