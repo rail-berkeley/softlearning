d0c25873d921d404c855f5388d66b1d8714a9b59
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-07-25 18:08:49 -0700

    Minor syntax updates and import fix for gym_env

--- softlearning/environments/gym_env.py
+++ softlearning/environments/gym_env.py
@@ -7,14 +7,13 @@ import traceback
 import logging
 
 try:
-    from gym.wrappers.monitoring import logger as monitor_logger
+    from gym.wrappers.monitor import logger as monitor_logger
 
     monitor_logger.setLevel(logging.WARNING)
 except Exception as e:
     traceback.print_exc()
 
 import os
-import os.path as osp
 from rllab.envs.base import Env, Step
 from rllab.core.serializable import Serializable
 from rllab.spaces.box import Box
@@ -57,11 +56,18 @@ class NoVideoSchedule(object):
 
 
 class GymEnv(Env, Serializable):
-    def __init__(self, env_name, record_video=False, video_schedule=None, log_dir=None, record_log=False,
+    def __init__(self,
+                 env_name,
+                 record_video=False,
+                 video_schedule=None,
+                 log_dir=None,
+                 record_log=False,
                  force_reset=True):
         if log_dir is None:
             if logger.get_snapshot_dir() is None:
-                logger.log("Warning: skipping Gym environment monitoring since snapshot_dir not configured.")
+                logger.log(
+                    "Warning: skipping Gym environment monitoring since"
+                    " snapshot_dir not configured.")
             else:
                 log_dir = os.path.join(logger.get_snapshot_dir(), "gym_log")
         Serializable.quick_init(self, locals())
@@ -87,14 +93,19 @@ class GymEnv(Env, Serializable):
             else:
                 if video_schedule is None:
                     video_schedule = CappedCubicVideoSchedule()
-            self.env = gym.wrappers.Monitor(self.env, log_dir, video_callable=video_schedule, force=True)
+            self.env = gym.wrappers.Monitor(
+                self.env,
+                log_dir,
+                video_callable=video_schedule,
+                force=True)
             self.monitoring = True
 
         self._observation_space = convert_gym_space(env.observation_space)
         logger.log("observation space: {}".format(self._observation_space))
         self._action_space = convert_gym_space(env.action_space)
         logger.log("action space: {}".format(self._action_space))
-        self._horizon = env.spec.tags['wrapper_config.TimeLimit.max_episode_steps']
+        self._horizon = env.spec.tags[
+            'wrapper_config.TimeLimit.max_episode_steps']
         self._log_dir = log_dir
         self._force_reset = force_reset
 
@@ -134,10 +145,10 @@ class GymEnv(Env, Serializable):
                 print("""
     ***************************
 
-    Training finished! You can upload results to OpenAI Gym by running the following command:
+    Training finished! You can upload results to OpenAI Gym by running the
+    following command:
 
     python scripts/submit_gym.py %s
 
     ***************************
                 """ % self._log_dir)
-
