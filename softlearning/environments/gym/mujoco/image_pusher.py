Fix image pusher image normalization divisor

softlearning/environments/gym/mujoco/image_pusher.py
@@ -15,7 +15,7 @@ class ImagePusherEnv(Pusher2dEnv):
     def _get_obs(self):
         width, height = self.image_size[:2]
         image = self.render(mode='rgb_array', width=width, height=height)
-        image = ((2.0 / 256.0) * image - 1.0)
+        image = ((2.0 / 255.0) * image - 1.0)
 
         return np.concatenate([
             image.reshape(-1),

softlearning/environments/rllab/image_pusher.py
@@ -18,7 +18,7 @@ class ImagePusherEnv(Pusher2dEnv):
     def get_current_obs(self):
         self.viewer_setup()
         image = self.render(mode='rgb_array')
-        image = (2.0 * image/256.0 - 1.0)
+        image = (2.0 * image/255.0 - 1.0)
 
         return np.concatenate([
             image.reshape(-1),