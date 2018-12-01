61505e268f24cba31a1fcc0d609172820e4176b2
Kristian Hartikainen <hartikainen@berkeley.edu>
2018-11-30 16:09:43 -0800

    Fix PicklableKerasModel.from_config

--- softlearning/utils/keras.py
+++ softlearning/utils/keras.py
@@ -22,3 +22,10 @@ class PicklableKerasModel(tf.keras.Model):
                     self.__class__.__name__: self.__class__})
 
         self.__dict__.update(loaded_model.__dict__.copy())
+
+    @classmethod
+    def from_config(cls, *args, custom_objects=None, **kwargs):
+        custom_objects = custom_objects or {}
+        custom_objects[cls.__name__] = cls
+        return super(PicklableKerasModel, cls).from_config(
+            *args, custom_objects=custom_objects, **kwargs)
