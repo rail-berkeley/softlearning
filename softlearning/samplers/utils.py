1711352a0627f5f9b644a28bc8bc9ef4c8e444c2
Kristian Hartikainen <hartikainen@berkeley.edu>
2018-12-21 10:50:05 -0800

    Update sampler rollout path to support env observation_keys

--- softlearning/samplers/utils.py
+++ softlearning/samplers/utils.py
@@ -75,7 +75,9 @@ def rollout(env,
 
     assert pool._size == t + 1
 
-    path = pool.batch_by_indices(np.arange(pool._size))
+    path = pool.batch_by_indices(
+        np.arange(pool._size),
+        observation_keys=getattr(env, 'observation_keys', None))
     path['infos'] = infos
 
     if render_mode == 'rgb_array':
