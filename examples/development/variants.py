78f215cf6f9da0e343dfc70b59874ff70244152c
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-02-01 21:50:14 -0800

    Fix SAC tau

--- examples/development/variants.py
+++ examples/development/variants.py
@@ -64,7 +64,7 @@ ALGORITHM_PARAMS_ADDITIONAL = {
             'reparameterize': REPARAMETERIZE,
             'lr': 3e-4,
             'target_update_interval': 1,
-            'tau': 1e-4,
+            'tau': 5e-3,
             'target_entropy': 'auto',
             'store_extra_policy_info': False,
             'action_prior': 'uniform',
