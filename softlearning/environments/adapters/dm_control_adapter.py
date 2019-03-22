1b22e39d684a1af151335836b42a3c1d79aad84c
Albert Yu <albertyu@berkeley.edu>
2019-03-21 23:12:48 -0700

    Implement convert_to_active_observation, step in dm_control_adapter.py

--- softlearning/environments/adapters/dm_control_adapter.py
+++ softlearning/environments/adapters/dm_control_adapter.py
@@ -1,9 +1,7 @@
 """Implements an adapter for DeepMind Control Suite environments."""
 
-
-# TODO(hartikainen): Need numpy? Import it here.
-
 from collections import OrderedDict
+
 import numpy as np
 from dm_control import suite
 from dm_control.rl.specs import ArraySpec, BoundedArraySpec
@@ -11,9 +9,11 @@ from gym import spaces
 
 from .softlearning_env import SoftlearningEnv
 
+
 # TODO(hartikainen): Add information of the available environments.
 # See `gym_adapter.py` for example.
 DM_CONTROL_ENVIRONMENTS = {}
+
 
 def convert_dm_control_to_gym_space(dm_control_space):
     if isinstance(dm_control_space, BoundedArraySpec):
@@ -26,15 +26,12 @@ def convert_dm_control_to_gym_space(dm_control_space):
             (gym_box.shape, dm_control_space.shape))
         return gym_box
     elif isinstance(dm_control_space, ArraySpec):
-        #convert to Box
         return spaces.Box(
             low=-float("inf"),
             high=float("inf"),
             shape=dm_control_space.shape,
             dtype=dm_control_space.dtype)
     elif isinstance(dm_control_space, OrderedDict):
-        #convert to Dict
-        #convert each value to box if it is an ArraySpec
         return spaces.Dict(OrderedDict([
             (key, convert_dm_control_to_gym_space(value))
             for key, value in dm_control_space.items()
@@ -42,8 +39,9 @@ def convert_dm_control_to_gym_space(dm_control_space):
     else:
         raise ValueError(dm_control_space)
 
+
 class DmControlAdapter(SoftlearningEnv):
-    """Adapter that implements the SoftlearningEnv for Gym envs."""
+    """Adapter that implements the SoftlearningEnv for DeepMind Control Suite envs."""
 
     def __init__(self,
                  domain,
@@ -70,7 +68,7 @@ class DmControlAdapter(SoftlearningEnv):
                 task_kwargs=kwargs
                 # TODO(hartikainen): Figure out how to pass kwargs to this guy.
                 # Need to split into `task_kwargs`, `environment_kwargs`, and `visualize_reward` bool.
-                # Check the suite.load(.) in: 
+                # Check the suite.load(.) in:
                 # https://github.com/deepmind/dm_control/blob/master/dm_control/suite/__init__.py
             )
         else:
@@ -107,48 +105,31 @@ class DmControlAdapter(SoftlearningEnv):
             "observation space and convert it's shape into a flattened shape.")
 
     def convert_to_active_observation(self, observation):
-        # TODO(hartikainen): Figure out how to convert the dictionary
-        # observation into a flattened numpy array. Something like:
-        # observation_keys = (
-        #     self.observation_keys
-        #     or list(self._env.observation_space.spaces.keys()))
-        #
-        # observation = np.concatenate([
-        #     observation[key] for key in observation_keys
-        # ], axis=-1)
-        #
-        # return observation
-
-        raise NotImplementedError(
-            "TODO(hartikainen): Figure out how to flatten the observation.")
+        flattened_observation = np.concatenate([
+            observation[key] for key in self.observation_keys])
+        return flattened_observation
 
     @property
     def action_space(self, *args, **kwargs):
-        # TODO(hartikainen): Figure out how to get the `action_space` from the
-        # `self._env`. In gym this is done by: `self._env.action_space`.
-        # Something like:
-        # action_space = self._env.action_space
-        # if len(action_space.shape) > 1:
-        #     raise NotImplementedError(
-        #         "Action space ({}) is not flat, make sure to check the"
-        #         " implemenation.".format(action_space))
-        # return action_space
         action_space = convert_dm_control_to_gym_space(self._env.action_spec())
+        if len(action_space.shape) > 1:
+            raise NotImplementedError(
+                "Action space ({}) is not flat, make sure to check the"
+                " implemenation.".format(action_space))
         return action_space
 
     def step(self, action, *args, **kwargs):
-        # TODO(hartikainen): this might of different format than the gym
-        # observation. Needs to return a tuple of:
-        #
-        # (observation, reward, done, info)
-        # Will be used (such as in simple_sampler.py): next_observation, reward, terminal, info = self.env.step(action)
-        return self._env.step(action, *args, **kwargs)
+        timestep = self._env.step(action, *args, **kwargs)
+        observation = timestep.observation
+        reward = timestep.reward
+        terminal = timestep.last()
+        info = {}
+        #TODO: Find how to extract info
+        return observation, reward, terminal, info
 
     def reset(self, *args, **kwargs):
         timestep = self._env.reset(*args, **kwargs)
-        flattened_observation = np.concatenate([
-            timestep.observation[key] for key in self.observation_keys])
-        return flattened_observation
+        return timestep.observation
 
     def render(self, *args, **kwargs):
         return self._env.render(*args, **kwargs)
