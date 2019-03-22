bf4eeeda3ad4eb91211edcca0c8211e85a54fe20
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-03-21 21:18:35 -0700

    Add tests for GymAdapter copy

--- tests/softlearning/environments/adapters/gym_adapter_test.py
+++ tests/softlearning/environments/adapters/gym_adapter_test.py
@@ -5,6 +5,12 @@ from softlearning.environments.adapters.gym_adapter import (
     GymAdapter, CUSTOM_GYM_ENVIRONMENTS)
 
 
+SKIP_ENVIRONMENTS = (
+    ('Pusher2d', 'ImageDefault-v0'),
+    ('Pusher2d', 'ImageReach-v0'),
+    ('Pusher2d', 'BlindReach-v0'))
+
+
 class TestGymAdapter(unittest.TestCase, AdapterTestClass):
     # TODO(hartikainen): This is a terrible way of testing the envs.
     # All the envs should be tested independently.
@@ -14,11 +20,6 @@ class TestGymAdapter(unittest.TestCase, AdapterTestClass):
 
     def test_environments(self):
         # Make sure that all the environments are creatable
-        SKIP_ENVIRONMENTS = (
-            ('Pusher2d', 'ImageDefault-v0'),
-            ('Pusher2d', 'ImageReach-v0'),
-            ('Pusher2d', 'BlindReach-v0'))
-
         def verify_reset_and_step(domain, task):
             env = GymAdapter(domain=domain, task=task)
             env.reset()
@@ -30,6 +31,44 @@ class TestGymAdapter(unittest.TestCase, AdapterTestClass):
                     continue
                 print("testing: ", domain, task)
                 verify_reset_and_step(domain, task)
+
+    def test_copy_environments(self):
+        domain, task = 'Swimmer', 'v3'
+        env_kwargs = {
+            'forward_reward_weight': 0,
+            'ctrl_cost_weight': 0,
+            'reset_noise_scale': 0,
+            'exclude_current_positions_from_observation': False,
+        }
+        env1 = self.create_adapter(domain=domain, task=task, **env_kwargs)
+        env1.reset()
+        env2 = env1.copy()
+
+        self.assertEqual(env1.observation_keys, env2.observation_keys)
+        for key, value in env_kwargs.items():
+            self.assertEqual(getattr(env1.unwrapped, f'_{key}'), value)
+            self.assertEqual(getattr(env2.unwrapped, f'_{key}'), value)
+
+        domain, task = 'HandReach', 'v0'
+        gym_adapter_kwargs = {
+            'observation_keys': ('observation', 'desired_goal')
+        }
+        env_kwargs = {
+            'distance_threshold': 0.123123,
+            'reward_type': 'dense',
+        }
+        env1 = self.create_adapter(
+            domain=domain, task=task, **gym_adapter_kwargs, **env_kwargs)
+        env1.reset()
+        env2 = env1.copy()
+
+        for key, value in gym_adapter_kwargs.items():
+            self.assertEqual(getattr(env1, key), value)
+            self.assertEqual(getattr(env2, key), value)
+
+        for key, value in env_kwargs.items():
+            self.assertEqual(getattr(env1.unwrapped, key), value)
+            self.assertEqual(getattr(env2.unwrapped, key), value)
 
     def test_environment_kwargs(self):
         env_kwargs = {
