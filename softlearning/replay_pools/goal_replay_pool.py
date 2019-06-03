f85f67c3049cbb228ae921e87cc2fb1a66654b6f
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-06-02 20:28:16 -0700

    Refactor GoalReplayPool.__init__
    * Now splits the goals and observations correctly

--- softlearning/replay_pools/goal_replay_pool.py
+++ softlearning/replay_pools/goal_replay_pool.py
@@ -1,18 +1,44 @@
 from gym.spaces import Dict
 
-from .flexible_replay_pool import Field
-from .simple_replay_pool import SimpleReplayPool
+from .flexible_replay_pool import FlexibleReplayPool, Field
 
 
-class GoalReplayPool(SimpleReplayPool):
+class GoalReplayPool(FlexibleReplayPool):
     def __init__(self,
                  environment,
+                 observation_fields=None,
+                 new_observation_fields=None,
                  *args,
+                 extra_fields=None,
                  **kwargs):
+        extra_fields = extra_fields or {}
         observation_space = environment.observation_space
+        action_space = environment.action_space
         assert isinstance(observation_space, Dict), observation_space
 
-        extra_fields = {
+        self._environment = environment
+        self._observation_space = observation_space
+        self._action_space = action_space
+
+        fields = {
+            'observations': {
+                name: Field(
+                    name=name,
+                    dtype=observation_space.dtype,
+                    shape=observation_space.shape)
+                for name, observation_space
+                in observation_space.spaces.items()
+                if name not in environment.goal_key_map.keys()
+            },
+            'next_observations': {
+                name: Field(
+                    name=name,
+                    dtype=observation_space.dtype,
+                    shape=observation_space.shape)
+                for name, observation_space
+                in observation_space.spaces.items()
+                if name not in environment.goal_key_map.keys()
+            },
             'goals': {
                 name: Field(
                     name=name,
@@ -20,9 +46,23 @@ class GoalReplayPool(SimpleReplayPool):
                     shape=observation_space.shape)
                 for name, observation_space
                 in observation_space.spaces.items()
-                if name in environment.goal_keys
+                if name in environment.goal_key_map.values()
             },
+            'actions': Field(
+                name='actions',
+                dtype=action_space.dtype,
+                shape=action_space.shape),
+            'rewards': Field(
+                name='rewards',
+                dtype='float32',
+                shape=(1, )),
+            # terminals[i] = a terminal was received at time i
+            'terminals': Field(
+                name='terminals',
+                dtype='bool',
+                shape=(1, )),
+            **extra_fields
         }
 
-        return super(GoalReplayPool, self).__init__(
-            environment, *args, **kwargs, extra_fields=extra_fields)
+        super(GoalReplayPool, self).__init__(
+            *args, fields=fields, **kwargs)
