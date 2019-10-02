668c0bc1f95fb6651d2f66bf3ffc1d5f58f5b639
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2019-10-02 16:28:56 +0300

    Replace random_three_vector with spherical

--- softlearning/utils/random.py
+++ softlearning/utils/random.py
@@ -1,14 +1,8 @@
 import numpy as np
 
 
-def random_three_vector():
-    """Generates a uniformly random 3D unit vector."""
-    phi = np.random.uniform(0, np.pi * 2)
-    costheta = np.random.uniform(-1, 1)
-
-    theta = np.arccos(costheta)
-    x = np.sin(theta) * np.cos(phi)
-    y = np.sin(theta) * np.sin(phi)
-    z = np.cos(theta)
-
-    return np.array((x,y,z))
+def spherical(size=(), ndim=2):
+    size = np.atleast_1d(size)
+    random_normal = np.random.standard_normal((ndim, *size))
+    normalized = random_normal / np.linalg.norm(random_normal, axis=0)
+    return normalized
