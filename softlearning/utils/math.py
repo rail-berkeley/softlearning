687637c1c2fdbc11c1516f6c25b5028e019a2be0
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2019-10-02 15:48:30 +0300

    Add math utils and random_three_vector

---
+++ softlearning/utils/math.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def random_three_vector():
+    """Generates a uniformly random 3D unit vector."""
+    phi = np.random.uniform(0, np.pi * 2)
+    costheta = np.random.uniform(-1, 1)
+
+    theta = np.arccos(costheta)
+    x = np.sin(theta) * np.cos(phi)
+    y = np.sin(theta) * np.sin(phi)
+    z = np.cos(theta)
+
+    return np.array((x,y,z))
