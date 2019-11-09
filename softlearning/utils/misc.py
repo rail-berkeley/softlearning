bba2e71cbdbeb9e4eacae17dc94820407acce0e2
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-11-09 10:48:13 -0800

    Remove unnecessary snapshot constants from utils

--- softlearning/utils/misc.py
+++ softlearning/utils/misc.py
@@ -9,10 +9,6 @@ PROJECT_PATH = os.path.dirname(
     os.path.realpath(os.path.join(__file__, '..', '..')))
 
 
-DEFAULT_SNAPSHOT_MODE = 'none'
-DEFAULT_SNAPSHOT_GAP = 1000
-
-
 def set_seed(seed):
     seed %= 4294967294
     random.seed(seed)
