0604044ce00496ba9ca8ffc2082a7dc60de3c6c1
nflu <nflugovoy@berkeley.edu>
2019-11-19 12:25:46 -0800

    better naming and set up script for point circle

--- examples/development/unconstrained_hyper_param_search/variants.py
+++ examples/development/unconstrained_hyper_param_search/variants.py
@@ -6,4 +6,7 @@ from examples.development import (
 def get_variant_spec(args):
     variant_spec = get_development_variant_spec(args)
     variant_spec['algorithm_params']['kwargs']['lr'] = tune.grid_search([1e-4, 3e-4, 5e-4])
+    variant_spec['algorithm_params']['kwargs']['tau'] = tune.grid_search([1e-3, 5e-3, 1e-2])
+    #variant_spec['algorithm_params']['sampler_params']['batch_size'] = tune.grid_search([128, 256, 512])
+    # TODO check how to set seed
     return variant_spec
--- experiments/point_circle_search.sh
+++ experiments/point_circle_search.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 source activate softlearning
 cd ~/softlearning/
-# TODO put in directory named by date
+# TODO put in directory and .out file named by date
 nohup softlearning run_example_local examples.development.unconstrained_hyper_param_search \
     --universe=gym \
-    --domain=PointGather \
+    --domain=PointCircle \
     --task=v0 \
-    --exp-name=sac_point_gather_unconstrained_hyper_param_search \
+    --exp-name=sac_point_circle_unconstrained_hyper_param_search \
     --num-samples=1 \
     --trial-cpus=1  \
-    --checkpoint-frequency=1000 > sac_point_gather_unconstrained_grid_search.out
+    --checkpoint-frequency=1000 > sac_point_circle_unconstrained_search.out
