f3b31ce40dfa240cfb03dabed9b8d2368ac5be73
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-12 18:45:50 +0000

    Replace tf.nest with tf.python.util.nest

--- softlearning/utils/tensorflow.py
+++ softlearning/utils/tensorflow.py
@@ -4,7 +4,10 @@ import tensorflow as tf
 
 
 if LooseVersion(tf.__version__) > LooseVersion("2.00"):
-    from tensorflow import nest
+    # We import tf.python.util.nest because the public tf.nest
+    # doesn't expose `map_structure_with_paths` method.
+    # TODO(hartikainen): Figure out a way to use regular tf.nest.
+    from tensorflow.python.util import nest
 else:
     from tensorflow.contrib.framework import nest
 
