ac8333646f057f378f41d1f42b252b39abe6acda
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-12 18:47:08 +0000

    Remove now-obsolete initialize_tf_variables

--- softlearning/utils/tensorflow.py
+++ softlearning/utils/tensorflow.py
@@ -12,22 +12,3 @@ else:
     from tensorflow.contrib.framework import nest
 
 
-def initialize_tf_variables(session, only_uninitialized=True):
-    variables = tf.compat.v1.global_variables() + tf.compat.v1.local_variables()
-
-    def is_initialized(variable):
-        try:
-            session.run(variable)
-            return True
-        except tf.errors.FailedPreconditionError:
-            return False
-
-        return False
-
-    if only_uninitialized:
-        variables = [
-            variable for variable in variables
-            if not is_initialized(variable)
-        ]
-
-    session.run(tf.compat.v1.variables_initializer(variables))
