94935988f91a855299d59bda231d9575e10aa47f
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-12 18:47:46 +0000

    Add cast_and_concat and apply_preprocessors to utils.tensorflow

--- softlearning/utils/tensorflow.py
+++ softlearning/utils/tensorflow.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tree
 
 
 def initialize_tf_variables(session, only_uninitialized=True):
@@ -20,3 +21,23 @@ def initialize_tf_variables(session, only_uninitialized=True):
         ]
 
     session.run(tf.compat.v1.variables_initializer(variables))
+
+
+def apply_preprocessors(preprocessors, inputs):
+    tree.assert_same_structure(inputs, preprocessors)
+    preprocessed_inputs = tree.map_structure(
+        lambda preprocessor, input_: (
+            preprocessor(input_) if preprocessor is not None else input_),
+        preprocessors,
+        inputs,
+    )
+
+    return preprocessed_inputs
+
+
+def cast_and_concat(x):
+    x = tree.map_structure(
+        lambda element: tf.cast(element, tf.float32), x)
+    x = tree.flatten(x)
+    x = tf.concat(x, axis=-1)
+    return x
