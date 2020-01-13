3b9c2ec491b01b14a621fc406e61d6c5977f2129
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-13 16:34:07 +0000

    Fix UniformPolicyTest

--- tests/softlearning/policies/uniform_policy_test.py
+++ tests/softlearning/policies/uniform_policy_test.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 from softlearning.policies.uniform_policy import ContinuousUniformPolicy
 from softlearning.environments.utils import get_environment
 
+from softlearning.utils.tensorflow import nest
+
 
 class ContinuousUniformPolicyTest(tf.test.TestCase):
     def setUp(self):
@@ -16,53 +18,30 @@ class ContinuousUniformPolicyTest(tf.test.TestCase):
             output_shape=self.env.action_shape,
             observation_keys=self.env.observation_keys)
 
-    def test_actions_and_log_pis_symbolic(self):
+    def test_actions_and_log_pis(self):
         observation1_np = self.env.reset()
         observation2_np = self.env.step(self.env.action_space.sample())[0]
 
-        observations_np = {}
-        for key in observation1_np.keys():
-            observations_np[key] = np.stack((
+        observations_np = type(observation1_np)((
+            (key, np.stack((
                 observation1_np[key], observation2_np[key]
-            )).astype(np.float32)
+            ), axis=0).astype(np.float32))
+            for key in observation1_np.keys()
+        ))
 
-        observations_tf = [tf.constant(x, dtype=tf.float32)
-                           for x in observations_np]
+        observations_tf = nest.map_structure(
+            lambda x: tf.constant(x, dtype=x.dtype), observations_np)
 
-        actions = self.policy.actions(observations_tf)
-        with self.assertRaises(NotImplementedError):
-            log_pis = self.policy.log_pis(observations_tf, actions)
+        for observations in (observations_np, observations_tf):
+            actions = self.policy.actions(observations)
+            with self.assertRaises(NotImplementedError):
+                log_pis = self.policy.log_pis(observations, actions)
 
-        self.assertEqual(actions.shape, (2, *self.env.action_shape))
-
-        self.evaluate(tf.compat.v1.global_variables_initializer())
-
-        actions_np = self.evaluate(actions)
-
-        self.assertEqual(actions_np.shape, (2, *self.env.action_shape))
-
-    def test_actions_and_log_pis_numeric(self):
-        observation1_np = self.env.reset()
-        observation2_np = self.env.step(self.env.action_space.sample())[0]
-
-        observations_np = {}
-        for key in observation1_np.keys():
-            observations_np[key] = np.stack((
-                observation1_np[key], observation2_np[key]
-            )).astype(np.float32)
-
-        actions_np = self.policy.actions(observations_np).numpy()
-        with self.assertRaises(NotImplementedError):
-            log_pis_np = self.policy.log_pis(observations_np, actions_np).numpy()
-
-        self.assertEqual(actions_np.shape, (2, *self.env.action_shape))
+            self.assertEqual(actions.shape, (2, *self.env.action_shape))
 
     def test_env_step_with_actions(self):
-        observation1_np = self.env.reset()
-        observations_np = {
-            key: value[None, :] for key, value in observation1_np.items()
-        }
-        action = self.policy.action(observations_np).numpy()
+        observation_np = self.env.reset()
+        action = self.policy.action(observation_np).numpy()
         self.env.step(action)
 
     def test_trainable_variables(self):
@@ -72,10 +51,12 @@ class ContinuousUniformPolicyTest(tf.test.TestCase):
         observation1_np = self.env.reset()
         observation2_np = self.env.step(self.env.action_space.sample())[0]
         observations_np = {}
-        for key in observation1_np.keys():
-            observations_np[key] = np.stack((
+        observations_np = type(observation1_np)((
+            (key, np.stack((
                 observation1_np[key], observation2_np[key]
-            )).astype(np.float32)
+            ), axis=0).astype(np.float32))
+            for key in observation1_np.keys()
+        ))
 
         diagnostics = self.policy.get_diagnostics(observations_np)
         self.assertTrue(isinstance(diagnostics, OrderedDict))
@@ -85,11 +66,12 @@ class ContinuousUniformPolicyTest(tf.test.TestCase):
         observation1_np = self.env.reset()
         observation2_np = self.env.step(self.env.action_space.sample())[0]
 
-        observations_np = {}
-        for key in observation1_np.keys():
-            observations_np[key] = np.stack((
+        observations_np = type(observation1_np)((
+            (key, np.stack((
                 observation1_np[key], observation2_np[key]
-            )).astype(np.float32)
+            ), axis=0).astype(np.float32))
+            for key in observation1_np.keys()
+        ))
 
         deserialized = pickle.loads(pickle.dumps(self.policy))
 
