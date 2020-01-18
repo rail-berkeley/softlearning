09fffe02f7d94c00c7d5a967ec231f2dcfc6e696
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-18 11:24:00 +0000

    Revert "Learn alpha variable directly"
    This reverts commit d64e05cdb0e606e5bac9fe1c615c936ff46d1315.

--- softlearning/algorithms/sac.py
+++ softlearning/algorithms/sac.py
@@ -134,7 +134,8 @@ class SAC(RLAlgorithm):
             learning_rate=self._policy_lr,
             name="policy_optimizer")
 
-        self._alpha = tf.Variable(1.0, name='alpha', dtype=tf.float32)
+        self._log_alpha = tf.Variable(0.0, name='log_alpha', dtype=tf.float32)
+        self._alpha = tfp.util.DeferredTensor(self._log_alpha, tf.exp)
         self._alpha_optimizer = tf.optimizers.Adam(
             self._alpha_lr, name='alpha_optimizer')
 
@@ -244,15 +245,16 @@ class SAC(RLAlgorithm):
 
         with tf.GradientTape() as tape:
             alpha_losses = -1.0 * (
-                self._alpha * tf.stop_gradient(log_pis + self._target_entropy))
+                self._log_alpha
+                * tf.stop_gradient(log_pis + self._target_entropy))
             # NOTE(hartikainen): It's important that we take the average here,
             # otherwise we end up effectively having `batch_size` times too
             # large learning rate.
             alpha_loss = tf.nn.compute_average_loss(alpha_losses)
 
-        alpha_gradients = tape.gradient(alpha_loss, [self._alpha])
+        alpha_gradients = tape.gradient(alpha_loss, [self._log_alpha])
         self._alpha_optimizer.apply_gradients(zip(
-            alpha_gradients, [self._alpha]))
+            alpha_gradients, [self._log_alpha]))
 
         return alpha_losses
 
@@ -281,7 +283,7 @@ class SAC(RLAlgorithm):
             ('Q_value-mean', tf.reduce_mean(Qs_values)),
             ('Q_loss-mean', tf.reduce_mean(Qs_losses)),
             ('policy_loss-mean', tf.reduce_mean(policy_losses)),
-            ('alpha', self._alpha),
+            ('alpha', self._alpha._value()),
             ('alpha_loss-mean', tf.reduce_mean(alpha_losses)),
         ))
         return diagnostics
@@ -309,7 +311,7 @@ class SAC(RLAlgorithm):
         """
 
         diagnostics = OrderedDict((
-            ('alpha', self._alpha.numpy()),
+            ('alpha', self._alpha._value().numpy()),
             ('policy', self._policy.get_diagnostics(
                 batch['observations'])),
         ))
@@ -327,7 +329,7 @@ class SAC(RLAlgorithm):
                 f'Q_optimizer_{i}': optimizer
                 for i, optimizer in enumerate(self._Q_optimizers)
             },
-            '_alpha': self._alpha,
+            '_log_alpha': self._log_alpha,
         }
 
         if hasattr(self, '_alpha_optimizer'):
