447e8c0c1f8ecb751f36c2edadce95abbee0096c
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-02-17 15:47:22 +0000

    Refactor SimpleReplayPool to work with Box observations

--- tests/softlearning/replay_pools/simple_replay_pool_test.py
+++ tests/softlearning/replay_pools/simple_replay_pool_test.py
@@ -1,3 +1,4 @@
+import pytest
 import unittest
 import numpy as np
 import gym
@@ -13,34 +14,88 @@ def create_pool(env, max_size=100):
 
 class SimpleReplayPoolTest(unittest.TestCase):
     def test_create_pool(self):
-        env = get_environment('gym', 'Swimmer', 'v3', {})
-        self.assertIsInstance(env.observation_space, gym.spaces.Dict)
+        ENVIRONMENTS = (
+            get_environment('gym', 'Swimmer', 'v3', {}),
+            gym.make('Swimmer-v3'),
+            gym.make('HandManipulateBlock-v0'),
+        )
+        for environment in ENVIRONMENTS:
+            pool = create_pool(env=environment, max_size=100)
+
+            def verify_field(field, expected_name, expected_dtype, expected_shape):
+                self.assertIsInstance(field, Field)
+                self.assertEqual(field.name, expected_name)
+                self.assertEqual(field.dtype, expected_dtype)
+                self.assertEqual(field.shape, expected_shape)
+                self.assertEqual(field.initializer, np.zeros)
+                self.assertEqual(field.default_value, 0.0)
+
+            if isinstance(environment.observation_space, gym.spaces.Dict):
+                self.assertIsInstance(pool.fields['observations'], dict)
+                for name, space in environment.observation_space.spaces.items():
+                    self.assertIn(name, pool.fields['observations'])
+                    field = pool.fields['observations'][name]
+                    verify_field(field, name, space.dtype, space.shape)
+
+            elif isinstance(environment.observation_space, gym.spaces.Box):
+                self.assertIsInstance(pool.fields['observations'], Field)
+                verify_field(field,
+                             'observations',
+                             environment.observation_space.dtype,
+                             environment.observation_space.shape)
+            else:
+                raise ValueError(environment.observation_space)
+
+            verify_field(
+                pool.fields['actions'],
+                'actions',
+                environment.action_space.dtype,
+                environment.action_space.shape)
+
+            verify_field(pool.fields['rewards'], 'rewards', 'float32', (1, ))
+            verify_field(pool.fields['terminals'], 'terminals', 'bool', (1, ))
+
+    def test_add_samples_box_observation(self):
+        env = gym.make('Swimmer-v3')
         pool = create_pool(env=env, max_size=100)
 
-        def verify_field(field, expected_name, expected_dtype, expected_shape):
-            self.assertIsInstance(field, Field)
-            self.assertEqual(field.name, expected_name)
-            self.assertEqual(field.dtype, expected_dtype)
-            self.assertEqual(field.shape, expected_shape)
-            self.assertEqual(field.initializer, np.zeros)
-            self.assertEqual(field.default_value, 0.0)
+        env.reset()
 
-        self.assertIsInstance(pool.fields['observations'], dict)
-        for name, space in env.observation_space.spaces.items():
-            self.assertIn(name, pool.fields)
-            field = pool.fields['observations'][name]
-            verify_field(field, name, space.dtype, space.shape)
+        num_samples = pool._max_size // 2
 
-        verify_field(
-            pool.fields['actions'],
-            'actions',
-            env.action_space.dtype,
-            env.action_space.shape)
+        samples = {
+            'observations': np.empty(
+                (num_samples, *env.observation_space.shape),
+                dtype=env.observation_space.dtype),
+            'next_observations': np.empty(
+                (num_samples, *env.observation_space.shape),
+                dtype=env.observation_space.dtype),
+            'actions': np.empty((num_samples, *env.action_space.shape)),
+            'rewards': np.empty((num_samples, 1), dtype=np.float32),
+            'terminals': np.empty((num_samples, 1), dtype=bool),
+        }
 
-        verify_field(pool.fields['rewards'], 'rewards', 'float32', (1, ))
-        verify_field(pool.fields['terminals'], 'terminals', 'bool', (1, ))
+        for i in range(num_samples):
+            action = env.action_space.sample()
+            observation, reward, terminal, info = env.step(action)
+            samples['observations'][i, :] = observation
+            samples['next_observations'][i, :] = observation
+            samples['actions'][i] = action
+            samples['rewards'][i] = reward
+            samples['terminals'][i] = terminal
 
-    def test_add_samples(self):
+        pool.add_path(samples)
+        last_n_batch = pool.last_n_batch(num_samples)
+        np.testing.assert_equal(
+            {
+                key: value
+                for key, value in last_n_batch.items()
+                if key not in
+                ('episode_index_backwards', 'episode_index_forwards')
+            },
+            samples)
+
+    def test_add_samples_dict_observation(self):
         env = get_environment('gym', 'Swimmer', 'v3', {})
         pool = create_pool(env=env, max_size=100)
 
