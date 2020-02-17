9dcfcdd5140a52f75be825b762c735f773ca1d12
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-02-17 10:21:42 +0000

    Replace tf.nest with tree

--- softlearning/utils/tensorflow.py
+++ softlearning/utils/tensorflow.py
@@ -2,14 +2,7 @@ from distutils.version import LooseVersion
 
 import tensorflow as tf
 
-
-if LooseVersion(tf.__version__) > LooseVersion("2.00"):
-    # We import tf.python.util.nest because the public tf.nest
-    # doesn't expose `map_structure_with_paths` method.
-    # TODO(hartikainen): Figure out a way to use regular tf.nest.
-    from tensorflow.python.util import nest
-else:
-    from tensorflow.contrib.framework import nest
+import tree as nest
 
 
 def initialize_tf_variables(session, only_uninitialized=True):
