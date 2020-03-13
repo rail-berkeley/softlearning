fa20f4ed8a8181aa67199347ec48d0aab690468b
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-03-13 22:52:38 +0000

    Resurrect development.main.ExperimentRunner saving/restoring

--- examples/development/main.py
+++ examples/development/main.py
@@ -3,8 +3,10 @@ import copy
 import glob
 import pickle
 import sys
+import json
 
 import tensorflow as tf
+import tree
 import ray
 from ray import tune
 
@@ -43,7 +45,6 @@ class ExperimentRunner(tune.Trainable):
 
     def _build(self):
         variant = copy.deepcopy(self._variant)
-
         environment_params = variant['environment_params']
         training_environment = self.training_environment = (
             get_environment_from_params(environment_params['training']))
@@ -106,52 +107,127 @@ class ExperimentRunner(tune.Trainable):
     def _pickle_path(self, checkpoint_dir):
         return os.path.join(checkpoint_dir, 'checkpoint.pkl')
 
-    def _replay_pool_pickle_path(self, checkpoint_dir):
+    def _algorithm_save_path(self, checkpoint_dir):
+        return os.path.join(checkpoint_dir, 'algorithm')
+
+    def _replay_pool_save_path(self, checkpoint_dir):
         return os.path.join(checkpoint_dir, 'replay_pool.pkl')
 
-    def _tf_checkpoint_prefix(self, checkpoint_dir):
-        return os.path.join(checkpoint_dir, 'checkpoint')
+    def _sampler_save_path(self, checkpoint_dir):
+        return os.path.join(checkpoint_dir, 'sampler.pkl')
 
-    def _get_tf_checkpoint(self):
-        tf_checkpoint = tf.compat.v1.train.Checkpoint(**self.algorithm.tf_saveables)
+    def _policy_save_path(self, checkpoint_dir):
+        return os.path.join(checkpoint_dir, 'policy')
 
-        return tf_checkpoint
+    def _save_replay_pool(self, checkpoint_dir):
+        if not self._variant['run_params'].get(
+                'checkpoint_replay_pool', False):
+            return
 
-    @property
-    def picklables(self):
-        return {
-            'variant': self._variant,
-            'training_environment': self.training_environment,
-            'evaluation_environment': self.evaluation_environment,
-            'sampler': self.sampler,
-            'algorithm': self.algorithm,
-            'policy_weights': self.policy.get_weights(),
-        }
+        replay_pool_save_path = self._replay_pool_save_path(checkpoint_dir)
+        self.replay_pool.save_latest_experience(replay_pool_save_path)
+
+    def _restore_replay_pool(self, current_checkpoint_dir):
+        if not self._variant['run_params'].get(
+                'checkpoint_replay_pool', False):
+            return
+
+        experiment_root = os.path.dirname(current_checkpoint_dir)
+
+        experience_paths = [
+            self._replay_pool_save_path(checkpoint_dir)
+            for checkpoint_dir in sorted(glob.iglob(
+                os.path.join(experiment_root, 'checkpoint_*')))
+        ]
+
+        for experience_path in experience_paths:
+            self.replay_pool.load_experience(experience_path)
+
+    def _save_sampler(self, checkpoint_dir):
+        with open(self._sampler_save_path(checkpoint_dir), 'wb') as f:
+            pickle.dump(self.sampler, f)
+
+    def _restore_sampler(self, checkpoint_dir):
+        with open(self._sampler_save_path(checkpoint_dir), 'rb') as f:
+            sampler = pickle.load(f)
+
+        self.sampler.__setstate__(sampler.__getstate__())
+        self.sampler.initialize(
+            self.training_environment, self.policy, self.replay_pool)
 
     def _save_value_functions(self, checkpoint_dir):
-        if isinstance(self.Qs, tf.keras.Model):
-            Qs = [self.Qs]
-        elif isinstance(self.Qs, (list, tuple)):
-            Qs = self.Qs
-        else:
-            raise TypeError(self.Qs)
-
-        for i, Q in enumerate(Qs):
-            checkpoint_path = os.path.join(checkpoint_dir, f'Qs_{i}')
+        tree.map_structure_with_path(
             # TODO(hartikainen/tf2): This should probably use tf2 format
-            Q.save_weights(checkpoint_path, save_format='h5')
+            lambda path, Q: Q.save_weights(
+                os.path.join(
+                    checkpoint_dir, '-'.join(('Q', *[str(x) for x in path]))),
+                save_format='tf'),
+            self.Qs)
 
     def _restore_value_functions(self, checkpoint_dir):
-        if isinstance(self.Qs, tf.keras.Model):
-            Qs = [self.Qs]
-        elif isinstance(self.Qs, (list, tuple)):
-            Qs = self.Qs
-        else:
-            raise TypeError(self.Qs)
+        tree.map_structure_with_path(
+            # TODO(hartikainen/tf2): This should probably use tf2 format
+            lambda path, Q: Q.load_weights(
+                os.path.join(
+                    checkpoint_dir, '-'.join(('Q', *[str(x) for x in path])))),
+            self.Qs)
 
-        for i, Q in enumerate(Qs):
-            checkpoint_path = os.path.join(checkpoint_dir, f'Qs_{i}')
-            Q.load_weights(checkpoint_path)
+    def _save_policy(self, checkpoint_dir):
+        save_path = self._policy_save_path(checkpoint_dir)
+        self.policy.save(save_path)
+
+    def _restore_policy(self, checkpoint_dir):
+        save_path = self._policy_save_path(checkpoint_dir)
+        status = self.policy.load_weights(save_path)
+        status.assert_consumed().run_restore_ops()
+
+    def _save_algorithm(self, checkpoint_dir):
+        save_path = self._algorithm_save_path(checkpoint_dir)
+
+        tf_checkpoint = tf.train.Checkpoint(**self.algorithm.tf_saveables)
+        tf_checkpoint.save(file_prefix=f"{save_path}/checkpoint")
+
+        state = self.algorithm.__getstate__()
+        # with open(f"{save_path}/state.json", 'w') as f:
+        with open(os.path.join(save_path, "state.json"), 'w') as f:
+            json.dump(state, f)
+
+    def _restore_algorithm(self, checkpoint_dir):
+        save_path = self._algorithm_save_path(checkpoint_dir)
+
+        # with open(f"{save_path}/state.json", 'r') as f:
+        with open(os.path.join(save_path, "state.json"), 'r') as f:
+            state = json.load(f)
+
+        self.algorithm.__setstate__(state)
+
+        # NOTE(hartikainen): We need to run one step on optimizers s.t. the
+        # variables get initialized.
+        # TODO(hartikainen): This should be done somewhere else.
+        tree.map_structure(
+            lambda Q_optimizer, Q: Q_optimizer.apply_gradients([
+                (tf.zeros_like(variable), variable)
+                for variable in Q.trainable_variables
+            ]),
+            tuple(self.algorithm._Q_optimizers),
+            tuple(self.Qs),
+        )
+
+        self.algorithm._alpha_optimizer.apply_gradients([(
+            tf.zeros_like(self.algorithm._log_alpha), self.algorithm._log_alpha
+        )])
+        self.algorithm._policy_optimizer.apply_gradients([
+            (tf.zeros_like(variable), variable)
+            for variable in self.policy.trainable_variables
+        ])
+
+        tf_checkpoint = tf.train.Checkpoint(**self.algorithm.tf_saveables)
+
+        status = tf_checkpoint.restore(tf.train.latest_checkpoint(
+            # os.path.split(f"{save_path}/checkpoint")[0])
+            # f"{save_path}/checkpoint-xxx"))
+            os.path.split(os.path.join(save_path, "checkpoint"))[0]))
+        status.assert_consumed().run_restore_ops()
 
     def _save(self, checkpoint_dir):
         """Implements the checkpoint logic.
@@ -166,108 +242,31 @@ class ExperimentRunner(tune.Trainable):
             `tf.train.Checkpoint` and `pickle.dump` in very unorganized way
             which makes things not so usable.
         """
-        pickle_path = self._pickle_path(checkpoint_dir)
-        with open(pickle_path, 'wb') as f:
-            pickle.dump(self.picklables, f)
-
+        self._save_replay_pool(checkpoint_dir)
+        self._save_sampler(checkpoint_dir)
         self._save_value_functions(checkpoint_dir)
-
-        if self._variant['run_params'].get('checkpoint_replay_pool', False):
-            self._save_replay_pool(checkpoint_dir)
-
-        tf_checkpoint = self._get_tf_checkpoint()
-
-        tf_checkpoint.save(
-            file_prefix=self._tf_checkpoint_prefix(checkpoint_dir))
+        self._save_policy(checkpoint_dir)
+        self._save_algorithm(checkpoint_dir)
 
         return os.path.join(checkpoint_dir, '')
 
-    def _save_replay_pool(self, checkpoint_dir):
-        replay_pool_pickle_path = self._replay_pool_pickle_path(
-            checkpoint_dir)
-        self.replay_pool.save_latest_experience(replay_pool_pickle_path)
-
-    def _restore_replay_pool(self, current_checkpoint_dir):
-        experiment_root = os.path.dirname(current_checkpoint_dir)
-
-        experience_paths = [
-            self._replay_pool_pickle_path(checkpoint_dir)
-            for checkpoint_dir in sorted(glob.iglob(
-                os.path.join(experiment_root, 'checkpoint_*')))
-        ]
-
-        for experience_path in experience_paths:
-            self.replay_pool.load_experience(experience_path)
-
     def _restore(self, checkpoint_dir):
         assert isinstance(checkpoint_dir, str), checkpoint_dir
-
         checkpoint_dir = checkpoint_dir.rstrip('/')
 
-        pickle_path = self._pickle_path(checkpoint_dir)
-        with open(pickle_path, 'rb') as f:
-            picklable = pickle.load(f)
+        self._build()
 
-        training_environment = self.training_environment = picklable[
-            'training_environment']
-        evaluation_environment = self.evaluation_environment = picklable[
-            'evaluation_environment']
-
-        replay_pool = self.replay_pool = (
-            get_replay_pool_from_variant(self._variant, training_environment))
-
-        if self._variant['run_params'].get('checkpoint_replay_pool', False):
-            self._restore_replay_pool(checkpoint_dir)
-
-        sampler = self.sampler = picklable['sampler']
-        Qs = self.Qs = get_Q_function_from_variant(
-            self._variant, training_environment)
+        self._restore_replay_pool(checkpoint_dir)
+        self._restore_sampler(checkpoint_dir)
         self._restore_value_functions(checkpoint_dir)
-        policy = self.policy = (
-            get_policy_from_variant(self._variant, training_environment))
-        self.policy.set_weights(picklable['policy_weights'])
-        initial_exploration_policy = self.initial_exploration_policy = (
-            get_policy_from_params(
-                self._variant['exploration_policy_params'],
-                training_environment))
-
-        self.algorithm = get_algorithm_from_variant(
-            variant=self._variant,
-            training_environment=training_environment,
-            evaluation_environment=evaluation_environment,
-            policy=policy,
-            initial_exploration_policy=initial_exploration_policy,
-            Qs=Qs,
-            pool=replay_pool,
-            sampler=sampler)
-        self.algorithm.__setstate__(picklable['algorithm'].__getstate__())
-
-        # We need to run one step on optimizers s.t. the variables get
-        # initialized.
-        for Q_optimizer, Q in zip(self.algorithm._Q_optimizers, Qs):
-            Q_optimizer.apply_gradients([
-                (tf.zeros_like(variable), variable)
-                for variable in Q.trainable_variables
-            ])
-        self.algorithm._alpha_optimizer.apply_gradients([
-            (tf.zeros_like(self.algorithm._alpha), self.algorithm._alpha)
-        ])
-        self.algorithm._policy_optimizer.apply_gradients([
-            (tf.zeros_like(variable), variable)
-            for variable in self.policy.trainable_variables
-        ])
-
-        tf_checkpoint = self._get_tf_checkpoint()
-        status = tf_checkpoint.restore(tf.compat.v1.train.latest_checkpoint(
-            os.path.split(self._tf_checkpoint_prefix(checkpoint_dir))[0]))
-
-        status.assert_consumed().run_restore_ops()
-
-        # TODO(hartikainen): target Qs should either be checkpointed or pickled.
-        for Q, Q_target in zip(self.algorithm._Qs, self.algorithm._Q_targets):
-            Q_target.set_weights(Q.get_weights())
+        self._restore_policy(checkpoint_dir)
+        self._restore_algorithm(checkpoint_dir)
 
         self._built = True
+
+        # # TODO(hartikainen): target Qs should either be checkpointed or pickled.
+        # for Q, Q_target in zip(self.algorithm._Qs, self.algorithm._Q_targets):
+        #     Q_target.set_weights(Q.get_weights())
 
 
 def main(argv=None):
